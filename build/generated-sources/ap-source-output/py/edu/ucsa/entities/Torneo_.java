0cd537d321b33d63a3d2a217973e4b2a3ba019b6
Enrique Rodriguez <erodriguez@vbaez-Aspire-5733.(none)>
2013-11-25 14:44:45 -0300

    primer commit

---
+++ build/generated-sources/ap-source-output/py/edu/ucsa/entities/Torneo_.java
@@ -0,0 +1,19 @@
+package py.edu.ucsa.entities;
+
+import java.util.Date;
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.2.0.v20110202-r8913", date="2013-11-25T09:58:25")
+@StaticMetamodel(Torneo.class)
+public class Torneo_ { 
+
+    public static volatile SingularAttribute<Torneo, Integer> id;
+    public static volatile SingularAttribute<Torneo, String> nombre;
+    public static volatile SingularAttribute<Torneo, Integer> nroEquipos;
+    public static volatile SingularAttribute<Torneo, Date> fehcaInicio;
+    public static volatile SingularAttribute<Torneo, Date> fechaFin;
+    public static volatile SingularAttribute<Torneo, Integer> anho;
+
+}
